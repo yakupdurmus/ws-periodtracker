68cd23a4c463462a98078a94c147347c5b8d3870
yakupdurmus <yakup.durmus@justlife.com>
2023-05-06 01:18:17 +0300

    vercel json added

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,8 +1,8 @@
 const db = {
-  host: "127.0.0.1",
-  user: "root",
-  password: "?Yakup5488",
-  database: "periodtrackerdb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
 
 const config = {
